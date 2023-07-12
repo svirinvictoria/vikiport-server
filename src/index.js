585c9cc89903db7747aedd3ab7888a0103e0fb0d
Vika <svirin.victoria@gmail.com>
2023-07-12 12:28:19 +0200

    .

--- src/index.js
+++ src/index.js
@@ -14,9 +14,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/test-route", (req, res) => {
-  res.json({
-    message: "Test route is working!",
-  });
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        message: "Test route is working!",
+    });
 });
 
 app.use("/api", router);
